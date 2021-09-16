a0d854d629f2575c0fb7aa81e17a0242231e264f
vikash1807 <86599432+vikash1807@users.noreply.github.com>
2021-09-16 19:44:40 +0530

    Add files via upload

---
+++ sizeofdatatypes.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "size of int is : " << sizeof(int) << endl;
+    cout << "size of float is : " << sizeof(float) << endl;
+    cout << "size of double is : " << sizeof(double) << endl;
+    cout << "size of char is : " << sizeof(char) << endl;
+    return 0;
+}
